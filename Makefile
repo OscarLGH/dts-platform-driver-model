3cc1977be786901517d079fbf619ee7cc3b87eb3
OscarLGH <OscarLGH@live.com>
2024-07-25 01:10:39 +0800

    Add user mode code.

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ obj-m += dts-platform-driver.o
 
 all:
 	make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -C $(KERNELDIR) M=$(PWD) modules KCFLAGS="-fno-asynchronous-unwind-tables -fno-unwind-tables"
-
+	riscv64-linux-gnu-gcc dts-platform-user.c -o dts-platform-user.out
 clean:
 	make -C $(KERNELDIR) M=$(PWD) clean
---
+++ dts-platform-user.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+
+#define PLATFORM_MODEL_IOCTL_MAGIC 0x5537
+#define PLATFORM_MODEL_IOCTL_SET_IRQFD	_IOW(PLATFORM_MODEL_IOCTL_MAGIC, 2, void *)
+#define PLATFORM_MODEL_IOCTL_SET_IRQ	_IOW(PLATFORM_MODEL_IOCTL_MAGIC, 3, void *)
+#define PLATFORM_MODEL_IOCTL_ALLOC_DMA	_IOW(PLATFORM_MODEL_IOCTL_MAGIC, 4, void *)
+#define PLATFORM_MODEL_IOCTL_FREE_DMA	_IOW(PLATFORM_MODEL_IOCTL_MAGIC, 5, void *)
+
+#define DEVICE_NAME "dts_platform_driver_dev_0"
+
+struct dma_region {
+    uint64_t dma_buffer_phys;
+	uint64_t unused;
+	uint64_t dma_buffer_size;
+};
+
+int main()
+{
+    int i;
+    int ret;
+    struct dma_region dma_regions[8] = {0};
+    uint64_t write_buffer[0x100] = {0};
+    int device_fd = open(DEVICE_NAME, O_RDWR);
+    if (device_fd < 0) {
+        printf("open device error!\n");
+        return -1;
+    }
+
+    //allocate 8 4K dma buffers.
+    for (i = 0; i < 8; i++) {
+        dma_regions[i].dma_buffer_size = 0x1000;
+        ret = ioctl(device_fd, PLATFORM_MODEL_IOCTL_ALLOC_DMA, &dma_regions[i]);
+        if (ret < 0) {
+            printf("ioctl error, ret = %d\n", ret);
+            return -1;
+        }
+        printf("dma buffer %d: 0x%llx\n", i, dma_regions[i].dma_buffer_phys);
+    }
+
+    ret = write(device_fd, write_buffer, 32);
+    if (ret < 0) {
+        printf("write error, ret = %d\n", ret);
+    }
+
+    close(device_fd);
+    return 0;
+}
+
